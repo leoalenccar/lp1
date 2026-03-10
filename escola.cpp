9d04844b999621dff2191ec3d72d19b546923047
leoalenccar <leoniuqalencar@gmail.com>
2026-03-10 17:54:59 -0300

    Continuação atividade escola.

--- escola.cpp
+++ escola.cpp
@@ -8,6 +8,44 @@ struct Aluno
     string nome;
     int idade;
     double notas[3];
+
+    Aluno() : id(""), nome(""), idade(0), notas{0,0,0} {}
+
+    Aluno(string id, string nome, int idade, double notas[3])
+    {
+        this->id = id;
+        this->nome = nome;
+        this->idade = idade;
+        for (int i = 0; i < 3; i++)
+        {
+            this->notas[i] = notas[i];
+        }
+    }
+
+    double calcular_media_aluno ()
+    {
+        double soma = 0;
+        for (int i = 0; i < 3; i++)
+        {
+            soma += notas[i];
+        }
+
+        return soma / 3;
+    }
+
+    void exibir_dados_aluno()
+    {
+        cout << id;
+        cout << nome;
+        cout << idade;
+
+        int contador = 1;
+        for (int i = 0; i < 3; i++)
+        {
+            cout << "nota " << contador << ": " << notas[i];
+            contador++;
+        }
+    }
 };
 
 class Escola
@@ -52,34 +90,78 @@ public:
         quantidade_alunos++;
     }
 
-    int calcular_media_aluno (Aluno aluno)
-    {
-        double soma = 0;
-        for (int i = 0; i < 3; i++)
-        {
-            soma += aluno.notas[i];
-        }
-
-        return soma / 3;
-    }
-
-    void exibir_dados_aluno (Aluno aluno)
-    {
-        cout << aluno.id;
-    }
-
     void exibir_dados_alunos ()
     {
-        for (int i = 0; i < quantidade_alunos; i++0)
+        for (int i = 0; i < quantidade_alunos; i++)
         {
-            cout 
+            Aluno a = dados[i];
+            a.exibir_dados_aluno();
         }
+    }
+
+    //tentar usar busca binária
+    void busca_por_ID (string codigo)
+    {
+        for (int i = 0; i < quantidade_alunos; i++)
+        {
+            if (codigo == dados[i].id)
+            {
+                dados[i].exibir_dados_aluno();
+            }
+        }
+    }
+
+    double media_turma ()
+    {
+        double contador = 0;
+        for (int i = 0; i < quantidade_alunos; i++)
+        {
+            Aluno a = dados[i];
+            contador += a.calcular_media_aluno();
+        }
+        return contador / quantidade_alunos;
     }
 
 };
 
+class Interface
+{
+    void mostrar_menu ()
+    {
+        cout << "O que deseja fazer?\n";
+        cout << "1 - Cadastrar aluno no sistema;\n";
+        cout << "2 - Exibir todos os alunos cadastrados;\n";
+        cout << "3 - Ver média de aluno;\n";
+        cout << "4 - Ver média geral da turma;\n";
+        cout << "5 - Buscar aluno por ID;\n";
+        cout << "0 - Encerrar.\n";
+    }
+};
+
+class Programar
+{
+    Interface ui;
+
+    
+
+    void Executar ()
+    {
+        while (true)
+        {
+            
+        }
+    }
+};
+
 int main ()
 {
+    Aluno a;
+
+    int array[3] = {0,2,3};
+
+    for (int i = 0; i < 3; i++){
+        a.notas[i] = array[i];
+    }
 
     return 0;
 }
