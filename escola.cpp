44f25f6feb30a415c44eae64bc1dbe8c53bd31ba
leoalenccar <leoniuqalencar@gmail.com>
2026-03-11 15:03:51 -0300

    Quase final.

--- escola.cpp
+++ escola.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
-#include <string>
+#include <thread>
+#include <chrono>
 using namespace std;
+
+enum Operacao
+{
+    ENCERRAR,
+    CADASTRAR,
+    DADOS_TODOS,
+    MEDIA_ALUNO,
+    MEDIA_TODOS,
+    BUSCAR_ID
+};
 
 struct Aluno
 {
@@ -35,14 +46,16 @@ struct Aluno
 
     void exibir_dados_aluno()
     {
-        cout << id;
-        cout << nome;
-        cout << idade;
+        cout << "\n";
+        cout << "ID: " << id << ";\n";
+        cout << "Nome: " << nome << ";\n";
+        cout << "Idade: " << idade << ";\n";
+        cout << "\n";
 
         int contador = 1;
         for (int i = 0; i < 3; i++)
         {
-            cout << "nota " << contador << ": " << notas[i];
+            cout << "nota " << contador << ": " << notas[i] << "\n";
             contador++;
         }
     }
@@ -86,6 +99,23 @@ public:
         {
             aumentarCapacidade();
         }
+
+        int nmr_id = quantidade_alunos + 1;
+        string novo_id;
+        if (quantidade_alunos + 1 < 10)
+        {
+            novo_id = "#00" + to_string(nmr_id);
+        }
+        else if (quantidade_alunos + 1 < 100)
+        {
+            novo_id = "#0" + to_string(nmr_id);
+        }
+        else
+        {
+            novo_id = "#" + to_string(nmr_id);
+        }
+
+        aluno.id = novo_id;
         dados[quantidade_alunos] = aluno;
         quantidade_alunos++;
     }
@@ -94,8 +124,9 @@ public:
     {
         for (int i = 0; i < quantidade_alunos; i++)
         {
-            Aluno a = dados[i];
-            a.exibir_dados_aluno();
+            dados[i].exibir_dados_aluno();
+            cout << "\n";
+            cout << "==========================\n";
         }
     }
 
@@ -107,25 +138,74 @@ public:
             if (codigo == dados[i].id)
             {
                 dados[i].exibir_dados_aluno();
+                break;
+            }
+            if (i == quantidade_alunos - 1 && codigo != dados[i].id)
+            {
+                cout << "Aluno não encontrado, tente novamente.\n";
             }
         }
+
     }
 
-    double media_turma ()
+
+    void buscar_media_aluno (string codigo)
     {
-        double contador = 0;
+        bool encontrado = false;
+
         for (int i = 0; i < quantidade_alunos; i++)
         {
-            Aluno a = dados[i];
-            contador += a.calcular_media_aluno();
+            if (codigo == dados[i].id)
+            {
+                double media = dados[i].calcular_media_aluno();
+
+                cout << "Aluno: " << dados[i].nome << ";\n";
+                cout << "ID: " << codigo << ";\n" << endl;
+
+                int contador = 1;
+                for (int j = 0; j < 3; j++)
+                {
+                    cout << "Nota " << contador << ": " << dados[i].notas[j] << ";\n";
+                    contador++;
+                }
+                cout << "Media: " << media << ".\n";
+
+                encontrado = true;
+                break;
+            }
         }
-        return contador / quantidade_alunos;
+
+        if (!encontrado)
+        {
+            cout << "Aluno não encontrado, tente novamente.\n";
+        }
     }
 
+    void media_turma()
+    {
+        if (quantidade_alunos == 0)
+        {
+            cout << "Nenhum aluno cadastrado.\n";
+        }
+        else
+        {
+
+            double contador = 0;
+            for (int i = 0; i < quantidade_alunos; i++)
+            {
+                Aluno a = dados[i];
+                contador += a.calcular_media_aluno();
+            }
+
+            double media = contador / quantidade_alunos;
+            cout << "Media da turma: " << media << endl;
+        }
+    }
 };
 
 class Interface
 {
+public:
     void mostrar_menu ()
     {
         cout << "O que deseja fazer?\n";
@@ -136,32 +216,140 @@ class Interface
         cout << "5 - Buscar aluno por ID;\n";
         cout << "0 - Encerrar.\n";
     }
+
+    void encerrar ()
+    {
+        cout << "Encerrando...";
+        this_thread::sleep_for(chrono::seconds(3));
+        cout << "\n";
+    }
+
+    Aluno novo_aluno ()
+    {
+        string nome;
+        int idade;
+        double notas[3];
+
+        cout << "Digite o nome do aluno: ";
+        cin >> nome;
+
+        cout << "\n";
+
+        cout << "Digite a idade do aluno: ";
+        cin >> idade;
+
+        int contador = 1;
+        for (int i = 0; i < 3; i++)
+        {
+            cout << "Digite a " << contador << "ª nota: ";
+            cin >> notas[i];
+            cout << "\n";
+            contador++;
+        }
+
+        Aluno aluno = {"000", nome, idade, notas};
+        return aluno;
+
+    }
+
+    string codigo ()
+    {
+        int id;
+        cout << "Digite o código ID do aluno: ";
+        cin >> id;
+        cout << "\n";
+
+        string codigo_id;
+
+        if (id < 10)
+        {
+            codigo_id = "#00" + to_string(id);
+        }
+        else if (id < 100)
+        {
+            codigo_id = "#0" + to_string(id);
+        }
+        else
+        {
+            codigo_id = "#" + to_string(id);
+        }
+
+        return codigo_id;
+    }
+
+    void invalido ()
+    {
+        cout << "Número inválido, tente novamente: ";
+    }
 };
 
-class Programar
+class Programa
 {
     Interface ui;
+    Escola escola;
 
+public:
+
+    void chamar_operacao (int operacao)
+    {
+        switch (operacao)
+        {
+            case ENCERRAR:
+                ui.encerrar();
+                break;
+
+            case CADASTRAR:
+                escola.cadastrarAluno(ui.novo_aluno());
+                break;
+
+            case DADOS_TODOS:
+                escola.exibir_dados_alunos();
+                break;
+
+            case MEDIA_ALUNO:
+                escola.buscar_media_aluno(ui.codigo());
+                break;
+
+            case MEDIA_TODOS:
+                escola.media_turma();
+                break;
+
+            case BUSCAR_ID:
+                escola.busca_por_ID(ui.codigo());
+                break;
+        }
+    }
     
 
-    void Executar ()
+    void executar ()
     {
         while (true)
         {
-            
+            ui.mostrar_menu();
+
+            int op;
+            cin >> op;
+            while (op < 0 || op > 5)
+            {
+                ui.invalido();
+                cin >> op;
+            }
+
+            chamar_operacao(op);
+
+            if (op == 0)
+            {
+                break;
+            }
         }
     }
 };
 
 int main ()
 {
-    Aluno a;
+    Programa p;
 
-    int array[3] = {0,2,3};
-
-    for (int i = 0; i < 3; i++){
-        a.notas[i] = array[i];
-    }
+    p.executar();
 
     return 0;
 }
